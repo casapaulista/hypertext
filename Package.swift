fdaf72217ee4f0ea04e40fd1eb4b29a9be462e6d
Thiago Campos <commit@thigcampos.com>
2025-08-05 14:26:29 -0300

    rename executable target to hx

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "Hypertext",
+            name: "hx",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "Mustache", package: "GRMustache.swift"),
