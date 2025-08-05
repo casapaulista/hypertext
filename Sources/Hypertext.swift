4faee396fd25e51c21f77afaa7df712cf6dc7c1b
Thiago Campos <commit@thigcampos.com>
2025-08-05 14:26:45 -0300

    include basic versions of expected commands

--- Sources/Hypertext.swift
+++ Sources/Hypertext.swift
@@ -8,7 +8,35 @@ import ArgumentParser
 
 @main
 struct Hypertext: ParsableCommand {
-    mutating func run() throws {
-        print("Hello, world!")
+    static let configuration = CommandConfiguration(
+        commandName: "hx",
+        abstract: "An elegant static site generator",
+        subcommands: [Init.self, Build.self, Serve.self]
+    )
+}
+
+extension Hypertext {
+    struct Init: ParsableCommand {
+        static let configuration = CommandConfiguration(abstract: "Create a new Hypertext project")
+        @Argument(help: "Name of the project")
+        var name: String
+
+        mutating func run() throws {
+            print("Hello, world!")
+        }
+    }
+    struct Build: ParsableCommand {
+        static let configuration = CommandConfiguration(
+            abstract: "Deletes the output directory if there is one and builds the site")
+        mutating func run() throws {
+            print("Hello, world!")
+        }
+    }
+    struct Serve: ParsableCommand {
+        static let configuration = CommandConfiguration(
+            abstract: "Serve the site. Rebuild and reload on change automatically")
+        mutating func run() throws {
+            print("Hello, world!")
+        }
     }
 }
